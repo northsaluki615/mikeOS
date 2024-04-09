7161a89a66e58fd2a9bf3f7beafa40d1fa420fc1
Michael Helmers <mikehelmers@proton.me>
2024-04-08 20:07:09 -0500

    Add favicon links and update metadata in base.njk layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -122,7 +122,6 @@ module.exports = function(eleventyConfig) {
 		outbox: true,
 		outboxCollection: 'posts'
 	  });
-
 	// Features to make your build faster (when you need them)
 
 	// If your passthrough copy gets heavy and cumbersome, add this line
